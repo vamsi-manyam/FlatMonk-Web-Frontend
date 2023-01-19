68cf5bc96b1b9ecd5db142d262cf77129b54a3a2
vamsi-manyam <39612961+vamsi-manyam@users.noreply.github.com>
2023-01-19 23:06:46 +0000

    coming soon

---
+++ flatmonk-web-fronted/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ComingSoon from './pages/ComingSoon';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ComingSoon/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ flatmonk-web-fronted/src/pages/ComingSoon.js
@@ -0,0 +1,19 @@
+
+
+function ComingSoon() {
+    return (
+      <div className="ComingSoon">
+        <div className="container">
+        <h1>
+          Website
+          <br />
+          Coming Soon
+        </h1>
+        {/* <Timer /> */}
+        {/* <Preloader /> */}
+      </div>
+      </div>
+    );
+  }
+
+export default ComingSoon  
